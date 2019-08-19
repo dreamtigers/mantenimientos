e3251f2734e4ebe9b9ddaecfdf0eaad6407b67b5
Raul <raulalfonzo66@gmail.com>
2019-08-19 16:52:41 -0400

    Sólo comentarios modificados acá

--- php/auth.php
+++ php/auth.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (!isset($_SESSION['user'])){
-        //First we check wether isAdmin exists, which means he is logged in
+        //First we check wether $_SESSION['user'] exists, which means he is logged in
         
         header('Location: index.php');
         echo 'You can not go there';
