e3b9f1c4960926385fd22f818f28355dd8e7afe2
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2019-08-24 09:45:59 -0400

    Add another database to better simulate prod env

---
+++ .docker/mysql/01-piston.sql
@@ -0,0 +1,81 @@
+-- Create additional db which simulates the prod env
+CREATE DATABASE IF NOT EXISTS `dbpistongps`;
+
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: dbpistongps.crg0q1tctueu.us-east-2.rds.amazonaws.com:3306
+-- Generation Time: Aug 24, 2019 at 10:14 AM
+-- Server version: 5.6.41-log
+-- PHP Version: 7.2.20-1+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpistongps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tc_users`
+--
+
+CREATE TABLE `dbpistongps`.`tc_users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `hashedpassword` varchar(128) DEFAULT NULL,
+  `salt` varchar(128) DEFAULT NULL,
+  `readonly` bit(1) NOT NULL DEFAULT b'0',
+  `administrator` bit(1) DEFAULT NULL,
+  `map` varchar(128) DEFAULT NULL,
+  `latitude` double NOT NULL DEFAULT '0',
+  `longitude` double NOT NULL DEFAULT '0',
+  `zoom` int(11) NOT NULL DEFAULT '0',
+  `twelvehourformat` bit(1) NOT NULL DEFAULT b'0',
+  `attributes` varchar(4000) DEFAULT NULL,
+  `coordinateformat` varchar(128) DEFAULT NULL,
+  `disabled` bit(1) DEFAULT b'0',
+  `expirationtime` timestamp NULL DEFAULT NULL,
+  `devicelimit` int(11) DEFAULT '-1',
+  `token` varchar(128) DEFAULT NULL,
+  `userlimit` int(11) DEFAULT '0',
+  `devicereadonly` bit(1) DEFAULT b'0',
+  `phone` varchar(128) DEFAULT NULL,
+  `limitcommands` bit(1) DEFAULT b'0',
+  `login` varchar(128) DEFAULT NULL,
+  `poilayer` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tc_users`
+--
+ALTER TABLE `dbpistongps`.`tc_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tc_users`
+--
+ALTER TABLE `dbpistongps`.`tc_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
